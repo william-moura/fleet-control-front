c1516400ec0473518b50ed6876a111ed489a8616
Will_Moura <william@febrafar.com.br>
2026-03-13 12:31:52 -0300

    protegendo rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,16 @@ import { KmComponent } from './pages/km-component/km-component';
 import { MaintenanceComponent } from './pages/maintenance-component/maintenance-component';
 import { FuelComponent } from './pages/fuel-component/fuel-component';
 import { ReportsComponent } from './pages/reports-component/reports-component';
+import { LoginComponent } from './pages/login-component/login-component';
+import { authGuard } from './auth-guard';
 
 export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent, data: { icon: 'dashboard', name: 'Dashboard' } },
-    { path: 'drivers', component: DriversComponent, data: { icon: 'groups', name: 'Motoristas' }},
-    { path: 'vehicles', component: Vehicles, data: { icon: 'local_shipping', name: 'Veículos' } },
-    { path: 'kilometers', component: KmComponent, data: { icon: 'speed', name: 'Kilometragem' } },
-    { path: 'maintenance', component: MaintenanceComponent, data: { icon: 'build', name: 'Manutenção' } },
-    { path: 'fuel', component: FuelComponent, data: { icon: 'local_gas_station', name: 'Combustível' } },
-    { path: 'reports', component: ReportsComponent, data: { icon: 'report', name: 'Relatórios' } },
+    { path: 'dashboard', component: DashboardComponent, data: { icon: 'dashboard', name: 'Dashboard' }, canActivate: [authGuard] },
+    { path: 'drivers', component: DriversComponent, data: { icon: 'groups', name: 'Motoristas' }, canActivate: [authGuard] },
+    { path: 'vehicles', component: Vehicles, data: { icon: 'local_shipping', name: 'Veículos' }, canActivate: [authGuard] },
+    { path: 'kilometers', component: KmComponent, data: { icon: 'speed', name: 'Kilometragem' }, canActivate: [authGuard] },
+    { path: 'maintenance', component: MaintenanceComponent, data: { icon: 'build', name: 'Manutenção' }, canActivate: [authGuard] },
+    { path: 'fuel', component: FuelComponent, data: { icon: 'local_gas_station', name: 'Combustível' }, canActivate: [authGuard] },
+    { path: 'reports', component: ReportsComponent, data: { icon: 'report', name: 'Relatórios' }, canActivate: [authGuard] },
+    { path: 'login', component: LoginComponent },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { LayoutService } from './services/layout-service';
 import { AsyncPipe } from '@angular/common';
@@ -11,5 +11,6 @@ import { AsyncPipe } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('fleet-control-front');
+  public router = inject(Router);
   constructor(public layoutService: LayoutService) {}
 }
