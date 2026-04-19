9f7859bcb761d1fd951564cab668a3441b5ff71d
WilliamMoura <williammoura908@gmail.com>
2026-04-18 21:09:58 -0300

    dashboard

--- src/app/pages/dashboard-component/dashboard-component.ts
+++ src/app/pages/dashboard-component/dashboard-component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard-service'; // Criaremos este serviço
+import { Dashboard } from '../../models/dashboard';
 
 @Component({
   selector: 'app-dashboard-component',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.scss',
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  private dashboardService = inject(DashboardService);
+  dashboard = signal<Dashboard | null>(null);
+  hoje = new Date();
+  
+  // Signals para os indicadores
+  totalVeiculosAtivos = signal(0);
+  consumoMedio = signal(0);
+  gastosMensais = signal(0);
+  abastecimentosRecentes = signal<any[]>([]);
+  manutencoes = signal<any[]>([]);
+  ngOnInit() {
+    this.dashboardService.getDashboard().subscribe((dashboard) => {
+      this.dashboard.set(dashboard);
+    });
+  }
 }
