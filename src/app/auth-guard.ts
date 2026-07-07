8bf4969eb41db00c80c63a2bae8fc972adec1216
Will_Moura <william@febrafar.com.br>
2026-07-07 09:42:06 -0300

    ajustes rotas

--- src/app/auth-guard.ts
+++ src/app/auth-guard.ts
@@ -5,6 +5,11 @@ import { AuthService } from './services/auth-service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    return true;
+  }
 
   if (authService.isAuthenticated()) {
     return true;
