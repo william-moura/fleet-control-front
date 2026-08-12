5d861ef42ec253f33805de5dea8d9f8b8f60cfb4
Will_Moura <william@febrafar.com.br>
2026-08-12 09:41:42 -0300

    ajustes

--- src/app/pages/add-update-orgao/add-update-orgao.ts
+++ src/app/pages/add-update-orgao/add-update-orgao.ts
@@ -61,12 +61,18 @@ export class AddUpdateOrgao {
   salvar() {
     if (this.form.get('nome')?.getError('required')) {
       this.form.get('nome')?.setErrors({required: true});
+      this.snackBar.open('O campo Nome é obrigatório', 'Fechar', { duration: 3000 });
+      return;
     }
     if (this.form.get('sigla')?.getError('required')) {
       this.form.get('sigla')?.setErrors({required: true});
+      this.snackBar.open('O campo Sigla é obrigatório', 'Fechar', { duration: 3000 });
+      return;
     }
     if (this.form.get('prefeituraId')?.getError('required')) {
       this.form.get('prefeituraId')?.setErrors({required: true});
+      this.snackBar.open('O campo Prefeitura é obrigatório', 'Fechar', { duration: 3000 });
+      return;
     }
     if (this.form.valid) {
       if (this.update()) {
